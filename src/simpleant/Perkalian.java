699058d19e8810e29ca8d5d64eda437e4984ead5
Zenko211 <firmansyahangga765@gmail.com>
2023-10-21 23:03:53 +0700

    Menambahkan beberapa file

---
+++ src/simpleant/Perkalian.java
@@ -0,0 +1,12 @@
+package simpleant; 
+public class Perkalian { 
+    public static void main(String[] args) { 
+        int a, b, hasil;
+        a = 20;
+        b = 150;
+        hasil = a * b;
+
+        System.out.println("Maka hasil perkalian dari 20 * 150 adalah ");
+        System.out.println(hasil);
+    } 
+}
